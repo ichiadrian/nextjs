1040078464517cec7551e9561e5319da350b7e57
ryanseptianto95 <ryanseptianto95@gmail>
2022-09-15 20:54:35 +0700

    update new

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class index extends React.Component {
+    
+    render(){
+        return(
+            "Hello world"
+        )
+    }
+}
+
+export default index;
---
+++ pages/indonesia.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class indonesia extends React.Component {
+    
+    render(){
+        return(
+            "Hello Indonesia"
+        )
+    }
+}
+
+export default indonesia;
